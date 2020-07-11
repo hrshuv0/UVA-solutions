71806f571672dd02dc931a974fdcb5711b9c590e
hrshuv0 <59235318+hrshuv0@users.noreply.github.com>
2020-07-12 01:44:18 +0600

    Create 10302 - Summation of Polynomials.cpp

---
+++ 10302 - Summation of Polynomials.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int x;
+
+    while(cin >> x)
+    {
+        cout << (x * (x+1) /2) * (x * (x+1) /2) << endl;
+    }
+
+    return 0;
+}
